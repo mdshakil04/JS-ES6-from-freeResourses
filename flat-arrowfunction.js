aeb2e18573896ad886124076f7597b3dd6aff151
mdshakil04 <m.shakil04@yahoo.com>
2025-06-11 20:57:34 +0600

    This keyword in arrow function

--- flat-arrowfunction.js
+++ flat-arrowfunction.js
@@ -4,4 +4,12 @@
     let number = (a , b) => {
         return a + b;
     }
-console.log(number(10, 5))
+// console.log(number(10, 5))
+let javaScript = {
+    name: 'JavaScript',
+    libraries: ["React", "Angular", "Vue"],
+    printLibraries: function (){
+        this.libraries.forEach((a) => console.log(`${this.name} loves ${a}`))
+    }
+}
+javaScript.printLibraries()
