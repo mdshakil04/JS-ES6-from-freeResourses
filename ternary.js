a8cec9c3856dc0ba94f451eaa701caccf7a2ca31
mdshakil04 <m.shakil04@yahoo.com>
2025-06-16 16:28:21 +0600

    ternary operator started

---
+++ ternary.js
@@ -0,0 +1,6 @@
+// Ternary operator
+//  condition : if condition is true this part will execute ? else this will execute
+//  let type = (age >= 18) ? 'adult' : 'child'
+let age = 16;
+let type = (age >= 18) ? 'adult' : 'child'
+console.log(type)
