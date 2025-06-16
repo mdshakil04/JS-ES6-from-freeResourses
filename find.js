310244d315d1a3fcfdc85b3b1ab5a385cd86d5f2
mdshakil04 <m.shakil04@yahoo.com>
2025-06-16 17:03:43 +0600

    Find Method done

---
+++ find.js
@@ -0,0 +1,7 @@
+let numbers = [2, 4, 6, 7, 8, 10, 12, 15];
+
+let targetedValue = numbers.find(function(currentValue){
+    return currentValue > 8;
+})
+// console.log(numbers)
+console.log(targetedValue)
